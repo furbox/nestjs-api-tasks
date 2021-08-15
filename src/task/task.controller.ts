0012dc1d1258ccbe7b0a0dcd01968509b9ffc10c
furbox <furbox@gmail.com>
2021-08-15 01:20:09 -0500

    edit update delete  task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -19,9 +19,30 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  create(@Body() TaskDTO: TaskDTO) {
-    return this.taskService.create(TaskDTO);
+  create(@Body() taskDTO: TaskDTO) {
+    return this.taskService.create(taskDTO);
   }
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() taskDTO: TaskDTO) {
+    return this.taskService.update(id, taskDTO);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskService.delete(id);
+  }
+
   //   @Post()
   //   method(@Body() body: any) {
   //     return body;
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -6,6 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class TaskService {
   tasks: ITask[] = [];
+
   create(TaskDTO: TaskDTO): ITask {
     const task = {
       id: uuidv4(),
@@ -13,5 +14,24 @@ export class TaskService {
     };
     this.tasks = [...this.tasks, task];
     return task;
+  }
+
+  findAll(): ITask[] {
+    return this.tasks;
+  }
+
+  findOne(id: string): ITask {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  update(id: string, taskDTO: TaskDTO): ITask {
+    const newTask = { id, ...taskDTO };
+    this.tasks = this.tasks.map((t) => (t.id === id ? newTask : t));
+    return newTask;
+  }
+
+  delete(id: string) {
+    this.tasks = this.tasks.filter((t) => t.id !== id);
+    return 'Tarea eliminada';
   }
 }
